d55345e2294c93d8ac0ddace4631a18225e33bd1
Jean Souza <jeancfsouza@gmail.com>
2022-10-26 21:43:32 -0300

    feat: finish basic functionalities

--- src/lib.rs
+++ src/lib.rs
@@ -3,28 +3,100 @@ use std::fs;
 
 mod input;
 
-pub fn run(unparsed_args: Vec<String>) -> Result<(), String> {
+pub fn run(unparsed_args: impl Iterator<Item = String>) -> Result<(), String> {
     let args = parse_args(unparsed_args)?;
-    if let Err(e) = read_file(args.file_path()) {
-        return Err(e.to_string());
+
+    let contents = match read_file(args.file_path()) {
+        Ok(res) => res,
+        Err(err) => return Err(err.to_string()),
+    };
+
+    let result = if args.should_ignore_case() {
+        search_case_insensitive(args.query(), &contents)
+    } else {
+        search_case_sensitive(args.query(), &contents)
+    };
+
+    if result.is_empty() {
+        println!("Nothing found");
+        return Ok(());
     }
+
+    for line in result {
+        println!("{line}");
+    }
+
     Ok(())
 }
 
-fn parse_args(mut unparsed_args: Vec<String>) -> Result<input::Args, String> {
-    if unparsed_args.len() != 3 {
-        return Err("Error! rgrep syntax is: rgrep <QUERY_STRING> <FILE_PATH>".to_owned());
-    }
-    let file_path = unparsed_args.remove(2);
-    let query = unparsed_args.remove(1);
+fn parse_args(mut unparsed_args: impl Iterator<Item = String>) -> Result<input::Args, String> {
+    unparsed_args.next();
+    let query = match unparsed_args.next() {
+        Some(arg) => arg,
+        None => return Err("Didn't get a query string".to_owned()),
+    };
+    let file_path = match unparsed_args.next() {
+        Some(arg) => arg,
+        None => return Err("Didn't get a file path".to_owned()),
+    };
 
     Ok(input::Args::new(file_path, query))
 }
 
-fn read_file(file_path: &str) -> Result<(), Box<dyn Error>> {
+fn read_file(file_path: &str) -> Result<String, Box<dyn Error>> {
     let contents = fs::read_to_string(file_path)?;
 
-    println!("With text:\n{contents}");
+    Ok(contents)
+}
 
-    Ok(())
+fn search<F>(contents: &str, f: F) -> Vec<&str>
+where
+    F: FnMut(&&str) -> bool,
+{
+    contents.lines().filter(f).collect()
+}
+
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    search(contents, |line| {
+        line.to_lowercase().contains(&query.to_lowercase())
+    })
+}
+
+fn search_case_sensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    search(contents, |line| line.contains(&query))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(
+            vec!["safe, fast, productive."],
+            search_case_sensitive(query, contents)
+        );
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,8 @@ use std::env;
 use std::process;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    rgrep::run(args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+    rgrep::run(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 }
