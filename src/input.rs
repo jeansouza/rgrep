95db67fc6935c172831402176868cac7813e42e8
Jean Souza <jeancfsouza@gmail.com>
2022-10-26 21:43:32 -0300

    feat: finish basic functionalities

--- src/input.rs
+++ src/input.rs
@@ -1,11 +1,19 @@
+use std::env;
+
 pub struct Args {
     file_path: String,
     query: String,
+    ignore_case: bool,
 }
 
 impl Args {
     pub fn new(file_path: String, query: String) -> Args {
-        Args { file_path, query }
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+        Args {
+            file_path,
+            query,
+            ignore_case,
+        }
     }
 
     pub fn file_path(&self) -> &String {
@@ -14,5 +22,9 @@ impl Args {
 
     pub fn query(&self) -> &String {
         &self.query
+    }
+
+    pub fn should_ignore_case(&self) -> bool {
+        self.ignore_case
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
     rgrep::run(args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 }
